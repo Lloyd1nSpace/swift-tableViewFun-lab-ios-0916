118e94b1f7c0d4ec6bab89ca2e4a0464d80b054f
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-12-28 18:41:21 -0500

    Super fun

--- Cats/Cats/ViewController.swift
+++ Cats/Cats/ViewController.swift
@@ -10,8 +10,59 @@ import UIKit
 
 class ViewController: UITableViewController {
     
+    var catBreeds = [String]()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        generateCatData()
+    }
+    
+    func generateCatData() {
+        catBreeds = ["Aegean",
+                     "Alpine Lynx",
+                     "American Lynx",
+                     "American Polydactyl",
+                     "Australian Tiffanie",
+                     "Bramble",
+                     "Burmilla Longhair (Tiffanie)",
+                     "Classicat (Jungala)",
+                     "Desert Lynx",
+                     "Dwelf",
+                     "Foldex cat",
+                     "Genetta",
+                     "Highlander (Highland Lynx)",
+                     "Jungle",
+                     "Jungle Curl",
+                     "Kucing Malaysia (Piawaian Kucing Malaysia)",
+                     "Lambkin (Nanus Rex)",
+                     "Mandalay",
+                     "Minuet (Napoleon)",
+                     "Mojave Spotted",
+                     "Owyhee Bob",
+                     "Pantherette",
+                     "Pudelkatze (Poodlecat)",
+                     "Punjabi (Punjabi Desert Cat)",
+                     "Russian Shorthair",
+                     "Safari",
+                     "Seltic",
+                     "Skookum",
+                     "St. Helens",
+                     "Stone Cougar",
+                     "Tasman Manx"]
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return catBreeds.count
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "KittyCell", for: indexPath)
+        cell.textLabel!.text = catBreeds[indexPath.row]
+        return cell
+    }
     
 }
